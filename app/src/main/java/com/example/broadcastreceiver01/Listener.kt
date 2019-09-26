0dd7acb85e0859c3b8efd1d2f338f24bd820f601
Henry AT <yiperu@gmail.com>
2019-09-25 23:16:47 -0300

    Add a Broadcast Receiver called Listener, config automatically to AndroidManifest

---
+++ app/src/main/java/com/example/broadcastreceiver01/Listener.kt
@@ -0,0 +1,13 @@
+package com.example.broadcastreceiver01
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class Listener : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("Listener.onReceive() is not implemented")
+    }
+}
