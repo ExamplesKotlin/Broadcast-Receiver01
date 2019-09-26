577641fa0558b52c7c3cbeafa431209fbd0b6582
Henry AT <yiperu@gmail.com>
2019-09-26 08:31:27 -0300

    02 example - programmatically, only when ACTION_AIRPLANE_MODE_CHANGED

--- app/src/main/java/com/example/broadcastreceiver01/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver01/MainActivity.kt
@@ -1,26 +1,46 @@
 package com.example.broadcastreceiver01
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listenerObject = Listener()
-        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+//        val listenerObject = Listener()
+//        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+//        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+//        registerReceiver(listenerObject, filter)
+
+        val filter = IntentFilter()
         filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-        registerReceiver(listenerObject, filter)
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                Toast.makeText(p0, intent?.action, Toast.LENGTH_SHORT).show()
+            }
+        }
+        registerReceiver(receiver, filter)
+
 
         button.setOnClickListener {
 
         }
 
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
